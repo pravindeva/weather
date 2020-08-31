8712993c5b7fd34cebc3c8d3531aec4a0c0a43b3
pravindeva <pravindeva2580@gmail.com>
2020-08-31 07:11:14 +0530

    Updates

---
+++ precache-manifest.6fc7cf817d15aa3de63ab6309f4b8b51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65fc664195f6455e2d06e2548401e62",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "ef201aaf55df911313e7",
+    "url": "/weather/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "38e336b8524baa107b47",
+    "url": "/weather/static/css/main.f54427d0.chunk.css"
+  },
+  {
+    "revision": "ef201aaf55df911313e7",
+    "url": "/weather/static/js/2.e522c111.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/weather/static/js/2.e522c111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e336b8524baa107b47",
+    "url": "/weather/static/js/main.4c235a0e.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
